89fad57b0b9459451bd95070d26b80f1a7c2ff58
plop <plop.dev0@gmail.com>
2025-02-05 17:00:55 +0000

    Refactor UDP server implementation to use socket directly, simplifying the handling of incoming data and client communication.

--- server.py
+++ server.py
@@ -1,23 +1,13 @@
-import socketserver
+import socket
 
+HOST, PORT = "localhost", 8240
 
-class MyUDPHandler(socketserver.BaseRequestHandler):
-    """
-    This class works similar to the TCP handler class, except that
-    self.request consists of a pair of data and client socket, and since
-    there is no connection the client address must be given explicitly
-    when sending data back via sendto().
-    """
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+    server_socket.bind((HOST, PORT))
+    print(f"UDP server listening on {HOST}:{PORT}")
 
-    def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        print("{} wrote:".format(self.client_address[0]))
-        print(data)
-        socket.sendto(data.upper(), self.client_address)
-
-
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 8240
-    with socketserver.UDPServer((HOST, PORT), MyUDPHandler) as server:
-        server.serve_forever()
+    while True:
+        data, client_address = server_socket.recvfrom(1024)  # Buffer size of 1024 bytes
+        print(f"{client_address[0]} wrote:")
+        print(data.decode())
+        server_socket.sendto(data, client_address)
